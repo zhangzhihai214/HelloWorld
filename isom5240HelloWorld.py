b09127f5aac45ce9f8260681da370b2f0e3ffc53
zhangzhihai214 <zhangzhihai214@gmail.com>
2026-02-14 17:23:40 +0800

    Update isom5240HelloWorld.py

--- isom5240HelloWorld.py
+++ isom5240HelloWorld.py
@@ -2,9 +2,7 @@ import streamlit as st
 from transformers import pipeline
 
 # Load the text classification model pipeline
-classifier = pipeline("text-classification",
-                      model='isom5240ust/bert-base-uncased-emotion',
-                      return_all_scores=True)
+classifier = pipeline("text-classification",model='isom5240ust/bert-base-uncased-emotion')
 
 # Streamlit application title
 st.title("Text Classification for you")
@@ -16,17 +14,9 @@ text = st.text_area("Enter the text to classify", "")
 # Perform text classification when the user clicks the "Classify" button
 if st.button("Classify"):
     # Perform text classification on the input text
-    results = classifier(text)[0]
+    result = classifier(text)[0]
 
     # Display the classification result
-    max_score = float('-inf')
-    max_label = ''
-
-    for result in results:
-        if result['score'] > max_score:
-            max_score = result['score']
-            max_label = result['label']
-
     st.write("Text:", text)
-    st.write("Label:", max_label)
-    st.write("Score:", max_score)
+    st.write("Label:", result['score'])
+    st.write("Score:", result['label'])
