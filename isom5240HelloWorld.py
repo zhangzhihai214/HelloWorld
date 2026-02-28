3938830321a8c866e6f22c2d3b4fce2eddcd9b46
zhangzhihai214 <zhangzhihai214@gmail.com>
2026-02-28 17:03:33 +0800

    Update isom5240HelloWorld.py

--- isom5240HelloWorld.py
+++ isom5240HelloWorld.py
@@ -8,7 +8,7 @@ st.header("Title: Age Classification using ViT")
 # Load the age classification pipeline
 # The code below should be placed in the main part of the program
 age_classifier = pipeline("image-classification", model="prithivMLmods/Age-Classification-SigLIP2")
-pipe("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/hub/parrots.png")
+
 
 image_name = "middleagedMan.jpg"
 image_name = Image.open(image_name).convert("RGB")
