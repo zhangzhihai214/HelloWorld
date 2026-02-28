d267ccbec58da1eaf5d57acb6f63decd4aebf004
zhangzhihai214 <zhangzhihai214@gmail.com>
2026-02-28 15:42:16 +0800

    Update isom5240HelloWorld.py

--- isom5240HelloWorld.py
+++ isom5240HelloWorld.py
@@ -1,24 +1,66 @@
+# Program title: Storytelling App
+
+# import part
 import streamlit as st
-from PIL import Image
-import time
+from transformers import pipeline
 
-# App title
-st.title("Streamlit Demo on Hugging Face")
+# function part
+# img2text
+def img2text(url):
+    image_to_text_model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    text = image_to_text_model(url)[0]["generated_text"]
+    return text
 
-# Write some text
-st.write("Welcome to a demo app showcasing basic Streamlit components!")
+# text2story
+def text2story(text):
+    pipe = pipeline("text-generation", model="pranavpsv/genre-story-generator-v2")
+    story_text = pipe(text)[0]['generated_text']
+    return story_text
 
-# File uploader for image and audio
-uploaded_image = st.file_uploader("Upload an image",
-                                  type=["jpg", "jpeg", "png"])
+# text2audio
+def text2audio(story_text):
+    pipe = pipeline("text-to-audio", model="Matthijs/mms-tts-eng")
+    audio_data = pipe(story_text)
+    return audio_data
 
-# Display image with spinner
-if uploaded_image is not None:
-    with st.spinner("Loading image..."):
-        time.sleep(1)  # Simulate a delay
-        image = Image.open(uploaded_image)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-# Button interaction
-if st.button("Click Me"):
-    st.write("🎉 You clicked the button!")
+def main():
+    st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
+    st.header("Turn Your Image to Audio Story")
+    uploaded_file = st.file_uploader("Select an Image...")
+
+    if uploaded_file is not None:
+        print(uploaded_file)
+        bytes_data = uploaded_file.getvalue()
+        with open(uploaded_file.name, "wb") as file:
+            file.write(bytes_data)
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+
+        #Stage 1: Image to Text
+        st.text('Processing img2text...')
+        scenario = img2text(uploaded_file.name)
+        st.write(scenario)
+
+        #Stage 2: Text to Story
+        st.text('Generating a story...')
+        story = text2story(scenario)
+        st.write(story)
+
+        #Stage 3: Story to Audio data
+        st.text('Generating audio data...')
+        audio_data =text2audio(story)
+
+        # Play button
+        if st.button("Play Audio"):
+            # Get the audio array and sample rate
+            audio_array = audio_data["audio"]
+            sample_rate = audio_data["sampling_rate"]
+
+            # Play audio directly using Streamlit
+            st.audio(audio_array,
+                     sample_rate=sample_rate)
+
+
+if __name__ == "__main__":
+    main()
